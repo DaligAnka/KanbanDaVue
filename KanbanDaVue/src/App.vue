984e47f4883f0599165f777e60fd21800798bdf5
DaligAnka <sony261298@gmail.com>
2023-10-10 15:23:59 +0300

    initialization

---
+++ KanbanDaVue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+   
+    </div>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
